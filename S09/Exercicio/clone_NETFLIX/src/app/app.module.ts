47a915411abf9830758c5f8b563fc0e231727ce2
CleiL <clei.lisboa@gmail.com>
2023-03-09 21:21:44 -0300

    criação do component menu e page login

--- S09/Exercicio/clone_NETFLIX/src/app/app.module.ts
+++ S09/Exercicio/clone_NETFLIX/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './components/card/card.component';
 import { SerieComponent } from './pages/serie/serie.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CardComponent,
-    SerieComponent
+    SerieComponent,
+    MenuComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
