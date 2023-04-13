f94368cc20c781764a430866fbe4c2166a2cef58
CleiL <clei.lisboa@gmail.com>
2023-04-13 07:33:01 -0300

    exercicio 02

--- Modulo02/S02/Exercicios/ex02/Program.cs
+++ Modulo02/S02/Exercicios/ex02/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ParOuImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro:");
+            int numero = Convert.ToInt32(Console.ReadLine()); // Lê o número digitado pelo usuário
+
+            if (numero % 2 == 0) // Verifica se o número é par
+            {
+                Console.WriteLine("O número " + numero + " é par.");
+            }
+            else
+            {
+                Console.WriteLine("O número " + numero + " é ímpar.");
+            }
+        }
+    }
+}
