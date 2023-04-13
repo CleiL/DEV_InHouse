9b80d2327b243651c1ee15eb6b5d27d344f6bdbd
CleiL <clei.lisboa@gmail.com>
2023-04-13 07:49:07 -0300

    exercício 06 semana 02

--- Modulo02/S02/Exercicios/ex06/Program.cs
+++ Modulo02/S02/Exercicios/ex06/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string frase = "A linguagem de programação C# é muito poderosa"; // Declaração da variável frase
+
+        List<string> palavras = new List<string>(); // Criação da lista de palavras
+
+        // Separando as palavras e adicionando-as na lista
+        string[] palavrasArray = frase.Split(' '); // Separando as palavras da frase usando espaço como delimitador
+        palavras.AddRange(palavrasArray); // Adicionando as palavras separadas na lista
+
+        // Exibindo apenas a palavra "poderosa"
+        string? palavraPoderosa = palavras.Find(palavra => palavra == "poderosa");
+        if (palavraPoderosa != null)
+        {
+            Console.WriteLine("Palavra encontrada: " + palavraPoderosa);
+        }
+        else
+        {
+            Console.WriteLine("Palavra não encontrada.");
+        }
+    }
+}
