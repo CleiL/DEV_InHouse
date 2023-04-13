046d33eb5209b5c4c1465c6b3c2115e3963994c3
CleiL <clei.lisboa@gmail.com>
2023-04-13 07:55:59 -0300

    exercicio 07 semana 02

--- Modulo02/S02/Exercicios/ex07/Program.cs
+++ Modulo02/S02/Exercicios/ex07/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero = 0;
+        bool numeroValido = false;
+
+        while (!numeroValido)
+        {
+            Console.Write("Digite um número inteiro positivo: ");
+            if (int.TryParse(Console.ReadLine(), out numero) && numero > 0)
+            {
+                numeroValido = true;
+            }
+            else
+            {
+                Console.WriteLine("Número inválido. Digite novamente.");
+            }
+        }
+
+        Console.WriteLine("Números pares de 0 até " + numero + ":");
+
+        for (int i = 0; i <= numero; i += 2)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.ReadLine();
+    }
+}
