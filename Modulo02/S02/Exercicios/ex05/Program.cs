c213d5d11f20cabc2f9cd51080b2e71b7c08fc7c
CleiL <clei.lisboa@gmail.com>
2023-04-13 07:43:26 -0300

    exercicio 5 semana 2

--- Modulo02/S02/Exercicios/ex05/Program.cs
+++ Modulo02/S02/Exercicios/ex05/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = new int[4]; // Declaração do array para armazenar os números
+
+        // Laço para ler os números do usuário e armazená-los no array
+        for (int i = 0; i < 4; i++)
+        {
+            Console.Write($"Digite o {i + 1}º número: ");
+            numeros[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Array.Sort(numeros); // Ordena o array em ordem crescente
+
+        Console.WriteLine("Números ordenados: ");
+        // Laço para imprimir os números ordenados na tela
+        for (int i = 0; i < 4; i++)
+        {
+            Console.WriteLine(numeros[i]);
+        }
+    }
+}
