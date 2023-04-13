c187ecda6aa9418e4a5b2eb8e8f995824b90b721
CleiL <clei.lisboa@gmail.com>
2023-04-13 07:34:16 -0300

    exercicio 04

--- Modulo02/S02/Exercicios/ex04/Program.cs
+++ Modulo02/S02/Exercicios/ex04/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace InformacoesCarro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a marca do carro:");
+            string marca = Console.ReadLine(); // Lê a marca do carro digitada pelo usuário
+
+            Console.WriteLine("Digite o modelo do carro:");
+            string modelo = Console.ReadLine(); // Lê o modelo do carro digitado pelo usuário
+
+            Console.WriteLine("Digite a quilometragem rodada do carro:");
+            int kmRodados = Convert.ToInt32(Console.ReadLine()); // Lê a quilometragem rodada do carro digitada pelo usuário
+
+            Console.WriteLine("Marca do carro: " + marca);
+            Console.WriteLine("Modelo do carro: " + modelo);
+            Console.WriteLine("Quilometragem rodada do carro: " + kmRodados + " km");
+
+            // Verifica se é necessário fazer revisão (quilometragem acima de 10.000 km)
+            if (kmRodados > 10000)
+            {
+                Console.WriteLine("É necessário fazer uma revisão.");
+            }
+        }
+    }
+}
