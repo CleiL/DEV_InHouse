091ed3ae72243a2062dab6519025ba933baa296b
CleiL <clei.lisboa@gmail.com>
2023-04-13 07:21:36 -0300

    exercicio01

--- Modulo02/S02/Exercicios/ex01/Program.cs
+++ Modulo02/S02/Exercicios/ex01/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SomaNumeros
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número inteiro:");
+            int numero1 = Convert.ToInt32(Console.ReadLine()); // Lê o primeiro número digitado pelo usuário
+
+            Console.WriteLine("Digite o segundo número inteiro:");
+            int numero2 = Convert.ToInt32(Console.ReadLine()); // Lê o segundo número digitado pelo usuário
+
+            int soma = numero1 + numero2; // Calcula a soma dos números
+
+            Console.WriteLine("A soma dos números é: " + soma); // Exibe a soma na tela
+        }
+    } 
+}
